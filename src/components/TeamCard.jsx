503e558b039c8630904598ab172836b4928634eb
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-26 18:01:20 +0200

    inizializza PokeApi

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -1,4 +1,21 @@
+
+let endPoint = "https://pokeapi.co/api/v2/pokemon/ditto"
+
+function getDitto(){
+    return(
+        axios.get(endPoint)
+            .then(response =>{
+                const ditto = response.data
+                console.log("siamo qua", ditto)
+                console.log
+            }).catch(error => 
+                console.error(error)
+            )
+    )
+}
+
 export default function TeamCard(){
+    getDitto()
     return(
             <div className="team-card">
                 
